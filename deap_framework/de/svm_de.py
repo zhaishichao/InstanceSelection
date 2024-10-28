44aa54a0a45d76574fbb2d07993cc6c023afde6d
zsc <2827636950@qq.com>
2024-10-28 22:15:15 +0800

    1、添加执行实例选择脚本，每次运行20次 2、使用matplot画柱状图

--- deap_framework/de/svm_de.py
+++ deap_framework/de/svm_de.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from tqdm import tqdm
 
 from deap_framework.de.fitness import objective_function
 
@@ -59,66 +60,67 @@ def cxBinomial(x, y, cr):
             x[i] = y[i]
     return x
 
-
 IND_DIM = x_train.shape[0]
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", array, typecode='d', fitness=creator.FitnessMin)
-
 toolbox = base.Toolbox()
 toolbox.register("attr_float", random.uniform, 0, 1)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
                  toolbox.attr_float, n=IND_DIM)
-
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
 toolbox.register("mutate", mutDE, f=0.8)
-
 toolbox.register("mate", cxBinomial, cr=0.8)
-
 toolbox.register("select", tools.selRandom, k=3)
-
 toolbox.register("evaluate", SVM_Error_Rate)
 
 
 def main():
     # Differential evolution parameters
     RUN = 20
-    NUM_POP = 50
-    NGEN = 100
-    x_train, x_test, y_train, y_test = train_test_split(dataset_x, dataset_y, test_size=0.3,
-                                                        random_state=np.random.randint(RUN))
-    pop = toolbox.population(n=NUM_POP);
-    hof = tools.HallOfFame(1)
+    with tqdm(total=RUN, desc="DE") as pbar:
+        for i in range(RUN):
+            NUM_POP = 50
+            NGEN = 100
+            x_train, x_test, y_train, y_test = train_test_split(dataset_x, dataset_y, test_size=0.3,
+                                                                random_state=np.random.randint(RUN))
+            pop = toolbox.population(n=NUM_POP);
+            hof = tools.HallOfFame(1)
 
-    # Evaluate the individuals
-    fitnesses = toolbox.map(toolbox.evaluate, pop)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
+            # Evaluate the individuals
+            fitnesses = toolbox.map(toolbox.evaluate, pop)
+            for ind, fit in zip(pop, fitnesses):
+                ind.fitness.values = fit
 
-    for g in range(1, NGEN):
-        children = []
-        for agent in pop:
-            # We must clone everything to ensure independence
-            a, b, c = [toolbox.clone(ind) for ind in toolbox.select(pop)]
-            x = toolbox.clone(agent)
-            y = toolbox.clone(agent)
-            y = toolbox.mutate(y, a, b, c)
-            z = toolbox.mate(x, y)
-            del z.fitness.values
-            children.append(z)
+            for g in range(1, NGEN):
+                children = []
+                for agent in pop:
+                    # We must clone everything to ensure independence
+                    a, b, c = [toolbox.clone(ind) for ind in toolbox.select(pop)]
+                    x = toolbox.clone(agent)
+                    y = toolbox.clone(agent)
+                    y = toolbox.mutate(y, a, b, c)
+                    z = toolbox.mate(x, y)
+                    del z.fitness.values
+                    children.append(z)
 
-        fitnesses = toolbox.map(toolbox.evaluate, children)
-        for (i, ind), fit in zip(enumerate(children), fitnesses):
-            ind.fitness.values = fit
-            if ind.fitness > pop[i].fitness:
-                pop[i] = ind
+                fitnesses = toolbox.map(toolbox.evaluate, children)
+                for (i, ind), fit in zip(enumerate(children), fitnesses):
+                    ind.fitness.values = fit
+                    if ind.fitness > pop[i].fitness:
+                        pop[i] = ind
 
-        hof.update(pop)
+                hof.update(pop)
 
-    print("Best individual is ", hof[0])
-    print("Best individual is ", len(hof[0]))
-    print("with fitness", hof[0].fitness.values[0])
+            print("Best individual is ", hof[0])
+            print("Best individual is ", len(hof[0]))
+            print("with fitness", hof[0].fitness.values[0])
+            pbar.set_postfix({
+                "当前迭代次数": i + 1,
+                "当前最高准确率": round(1 - hof[0].fitness.values[0], 3)
+            })
+            # 更新进度条
+            pbar.update(1)
 
 
 if __name__ == "__main__":
