1c9010c0a5830e99d161cebf123a79d507041d9f
zsc <2827636950@qq.com>
2025-01-02 23:56:37 +0800

    将所有参数，保存到parameter.py中

---
+++ instance_selection/parameter/parameter.py
@@ -0,0 +1,46 @@
+# MLP参数设置
+N_SPLITS = 5  # k-fold交叉验证的折数
+LEARNING_RATE = 0.1  # 学习率
+RANDOM_SEED = 42
+
+# 遗传算法迭代参数设置
+NGEN = 30  # 迭代次数
+POPSIZE = 40  # 种群数量
+CXPB = 1.0  # 交叉因子/交叉率
+MR = 0.2  # 突变因子/突变率
+
+# 各个数据集参数
+Satellite = {
+    'DATASETNAME': 'Satellite.mat',
+    'HIDDEN_SIZE': 15,
+    'MAX_ITER': 100,
+    'LEARNING_RATE': 0.1
+}
+
+Nursery = {
+    'DATASETNAME': 'Nursery.mat',
+    'HIDDEN_SIZE': 20,
+    'MAX_ITER': 100,
+    'LEARNING_RATE': 0.1
+}
+
+Contraceptive = {
+    'DATASETNAME': 'Contraceptive.mat',
+    'HIDDEN_SIZE': 15,
+    'MAX_ITER': 200,
+    'LEARNING_RATE': 0.1
+}
+
+WallRobot = {
+    'DATASETNAME': 'WallRobot.mat',
+    'HIDDEN_SIZE': 20,
+    'MAX_ITER': 200,
+    'LEARNING_RATE': 0.1
+}
+
+Chess = {
+    'DATASETNAME': 'Chess.mat',
+    'HIDDEN_SIZE': 20,
+    'MAX_ITER': 200,
+    'LEARNING_RATE': 0.1
+}
