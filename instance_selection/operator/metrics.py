8c3db799fca1dfb15bc672913b142cf7328e2b0f
zsc <2827636950@qq.com>
2025-02-17 20:38:34 +0800

    解决了计算mAUC时不能计算二分类的AUC指标的问题，是由于sklearn库提供的roc_auc_score函数，对二分类和多分类问题的AUC计算方式不同，需要传入不同的参数

--- instance_selection/operator/metrics.py
+++ instance_selection/operator/metrics.py
@@ -49,7 +49,12 @@ def fitness_accuracy(individual, weights_train):
 ######################################
 def calculate_gmean_mauc(y_pred_proba, y):
     # 计算 ROC AUC（ovo+macro）
-    auc_ovo_macro = roc_auc_score(y, y_pred_proba, multi_class="ovo", average="macro")
+    # 统计y中有多少类
+    num_classes = len(np.unique(y))
+    if num_classes == 2:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba[:, 1])
+    else:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba, multi_class="ovo", average="macro")
     y_pred = np.argmax(y_pred_proba, axis=1)
     cm = confusion_matrix(y, y_pred)
     # 计算每类召回率（每类正确预测个数 / 该类总数）
