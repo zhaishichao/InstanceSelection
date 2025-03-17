00bc441d883992571cf61b80a51131094d12c94d
zsc <2827636950@qq.com>
2025-03-17 11:00:34 +0800

    重构了数据集的参数配置信息：（1）去除了先前的字典形式的参数配置（2）创建了DatasetConfig参数配置类，简化繁琐的参数配置过程

--- instance_selection/parameter/parameter.py
+++ instance_selection/parameter/parameter.py
@@ -12,115 +12,31 @@ POPSIZE = 30  # 种群数量
 CXPB = 1.0  # 交叉因子/交叉率
 MR = 0.2  # 突变因子/突变率
 STOP_SIGN = 5  # 停止标志
-# 各个数据集参数
-Satellite = {
-    'DATASETNAME': 'Satellite.mat',
-    'HIDDEN_SIZE': 15,
-    'MAX_ITER': 100,
-    'LEARNING_RATE': 0.1
-}
 
-Nursery = {
-    'DATASETNAME': 'Nursery.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 100,
-    'LEARNING_RATE': 0.1
-}
+# 设置一个类来保存数据集的配置信息
+class DatasetConfig:
+    def __init__(self, dataset_name, hidden_size, max_iter, learning_rate):
+        self.DATASETNAME = dataset_name
+        self.HIDDEN_SIZE = hidden_size
+        self.MAX_ITER = max_iter
+        self.LEARNING_RATE = learning_rate
 
-Contraceptive = {
-    'DATASETNAME': 'Contraceptive.mat',
-    'HIDDEN_SIZE': 15,
-    'MAX_ITER': 200,
-    'LEARNING_RATE': 0.1
-}
-
-WallRobot = {
-    'DATASETNAME': 'WallRobot.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 200,
-    'LEARNING_RATE': 0.1
-}
-
-Car = {
-    'DATASETNAME': 'Car.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 200,
-    'LEARNING_RATE': 0.1
-}
-
-Balance_Scale = {
-    'DATASETNAME': 'Balance_Scale.mat',
-    'HIDDEN_SIZE': 15,
-    'MAX_ITER': 500,
-    'LEARNING_RATE': 0.1
-}
-
-Ecoli = {
-    'DATASETNAME': 'Ecoli.mat',
-    'HIDDEN_SIZE': 5,
-    'MAX_ITER': 200,
-    'LEARNING_RATE': 0.1
-}
-
-Splice = {
-    'DATASETNAME': 'Splice.mat',
-    'HIDDEN_SIZE': 5,
-    'MAX_ITER': 100,
-    'LEARNING_RATE': 0.1
-}
-
-Glass = {
-    'DATASETNAME': 'Glass.mat',
-    'HIDDEN_SIZE': 10,
-    'MAX_ITER': 2000,
-    'LEARNING_RATE': 0.1
-}
-
-Dermatology = {
-    'DATASETNAME': 'Dermatology.mat',
-    'HIDDEN_SIZE': 2,
-    'MAX_ITER': 1000,
-    'LEARNING_RATE': 0.1
-}
-
-Page_Blocks = {
-    'DATASETNAME': 'Page_Blocks.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 100,
-    'LEARNING_RATE': 0.1
-}
-
-Pen_Digits = {
-    'DATASETNAME': 'Pen_Digits.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 100,
-    'LEARNING_RATE': 0.1
-}
-
-Abalone = {
-    'DATASETNAME': 'Abalone.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 500,
-    'LEARNING_RATE': 0.1
-}
-
-Chess = {
-    'DATASETNAME': 'Chess.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 200,
-    'LEARNING_RATE': 0.1
-}
-
-Letter = {
-    'DATASETNAME': 'Letter.mat',
-    'HIDDEN_SIZE': 10,
-    'MAX_ITER': 1000,
-    'LEARNING_RATE': 0.1
-}
-
-German = {
-    'DATASETNAME': 'German.mat',
-    'HIDDEN_SIZE': 20,
-    'MAX_ITER': 500,
-    'LEARNING_RATE': 0.1
-}
+# 定义数据集配置
+Satellite = DatasetConfig('Satellite.mat', 15, 100, 0.1)
+Nursery = DatasetConfig('Nursery.mat', 20, 100, 0.1)
+Contraceptive = DatasetConfig('Contraceptive.mat', 15, 200, 0.1)
+WallRobot = DatasetConfig('WallRobot.mat', 20, 200, 0.1)
+Car = DatasetConfig('Car.mat', 20, 200, 0.1)
+Balance_Scale = DatasetConfig('Balance_Scale.mat', 15, 500, 0.1)
+Ecoli = DatasetConfig('Ecoli.mat', 5, 200, 0.1)
+Splice = DatasetConfig('Splice.mat', 5, 100, 0.1)
+Glass = DatasetConfig('Glass.mat', 10, 2000, 0.1)
+Dermatology = DatasetConfig('Dermatology.mat', 2, 1000, 0.1)
+Page_Blocks = DatasetConfig('Page_Blocks.mat', 20, 100, 0.1)
+Pen_Digits = DatasetConfig('Pen_Digits.mat', 20, 100, 0.1)
+Abalone = DatasetConfig('Abalone.mat', 20, 500, 0.1)
+Chess = DatasetConfig('Chess.mat', 20, 200, 0.1)
+Chess2 = DatasetConfig('Chess2.mat', 20, 200, 0.1)
+Letter = DatasetConfig('Letter.mat', 10, 1000, 0.1)
+German = DatasetConfig('German.mat', 20, 500, 0.1)
+Yeast = DatasetConfig('Yeast.mat', 20, 500, 0.1)
