9c5246e7a2074c27b623f36b192070bbdef6450d
zsc <2827636950@qq.com>
2025-03-21 09:58:38 +0800

    调用imbalanced-ensemble库，实现SPE结合MLP模型（2020）

---
+++ metrics/metrics.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.stats import gmean
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.base import clone
+
+def calculate_gmean_mauc(y_pred_proba, y):
+    # 计算 ROC AUC（ovo+macro）
+    # 统计y中有多少类
+    num_classes = len(np.unique(y))
+    if num_classes == 2:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba[:, 1])
+    else:
+        auc_ovo_macro = roc_auc_score(y, y_pred_proba, multi_class="ovo", average="macro")
+    y_pred = np.argmax(y_pred_proba, axis=1)
+    cm = confusion_matrix(y, y_pred)
+    # 计算每类召回率（每类正确预测个数 / 该类总数）
+    recall_per_class = cm.diagonal() / cm.sum(axis=1)
+    # 计算G-Mean
+    geometric_mean = gmean(recall_per_class)
+    return round(geometric_mean, 6), round(auc_ovo_macro, 6), recall_per_class
